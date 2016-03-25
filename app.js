ab2993b05fc5b951f409c4ca46629862ef13dc0b
Ryan Flaherty <rcflaherty@gmail.com>
2016-03-25 15:36:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/db');
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+let router = express.Router();
+require(__dirname + '/routes/userRoutes')(router);
+require(__dirname + '/routes/login')(router);
+app.use('/', router);
+
+app.listen(port, function() {
+  console.log('Server listening on port ' + (port || 3000));
+});
+
